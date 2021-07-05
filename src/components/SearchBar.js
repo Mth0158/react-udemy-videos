0e4a72b0bd8e7b7d0300e2c892f89ab31c99a4e2
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-07-05 10:49:00 +0200

    Refactored SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,51 @@
 import "./SearchBar.css";
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+const SearchBar = ({ onTermSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    this.props.onTermSubmit(this.state.term);
+    onTermSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar">
-        <form onSubmit={this.onFormSubmit}>
-          <label>Search</label>
-          <input
-            type="text"
-            value={this.state.term}
-            onChange={(e) => this.setState({ term: e.target.value })}
-          />
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar">
+      <form onSubmit={onSubmit}>
+        <label>Search</label>
+        <input
+          type="text"
+          value={term}
+          onChange={(event) => setTerm(event.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
+
+// Class component
+// class SearchBar extends React.Component {
+//   state = { term: "" };
+
+//   onFormSubmit = (event) => {
+//     event.preventDefault();
+//     this.props.onTermSubmit(this.state.term);
+//   };
+
+//   render() {
+//     return (
+//       <div className="search-bar">
+//         <form onSubmit={this.onFormSubmit}>
+//           <label>Search</label>
+//           <input
+//             type="text"
+//             value={this.state.term}
+//             onChange={(e) => this.setState({ term: e.target.value })}
+//           />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 export default SearchBar;
